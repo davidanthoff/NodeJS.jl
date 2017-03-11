f296b0058d1dc676aec95129a465e687df905769
David Anthoff <anthoff@berkeley.edu>
2017-03-10 22:20:33 -0800

    Add initial version of run_node

--- src/NodeJS.jl
+++ src/NodeJS.jl
@@ -1,5 +1,12 @@
 module NodeJS
 
-# package code goes here
+using BinDeps
+
+@BinDeps.load_dependencies
+
+function run_node(cmd::AbstractString)
+    node_path = BinDeps.node[1][2]
+    run(`$node_path $cmd`)
+end
 
 end # module
