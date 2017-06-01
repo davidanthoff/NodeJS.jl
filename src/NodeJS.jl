69d3737ac67df5aa308f63636a0c78e2714a53ee
David Anthoff <anthoff@berkeley.edu>
2017-06-01 09:54:21 -0700

    Reenable npm_cmd

--- src/NodeJS.jl
+++ src/NodeJS.jl
@@ -2,7 +2,7 @@ module NodeJS
 
 using Compat
 
-export nodejs_cmd, npm_script
+export nodejs_cmd, npm_cmd
 
 depsjl = joinpath(@__DIR__, "..", "deps", "deps.jl")
 if !isfile(depsjl)
@@ -15,14 +15,22 @@ end
 Return the full path of the node command.
 """
 function nodejs_cmd()
-    return node_executable_path
+    return `$node_executable_path`
+end
+
+"""
+Return the full path of the npm script.
+"""
+function npm_script()
+    return npm_script_path
 end
 
 """
 Return the full path of the npm command.
 """
-function npm_script()
-    return npm_script_path
+function npm_cmd()
+    return Cmd(`$(nodejs_cmd()) $(npm_script())`)
 end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,4 @@ using NodeJS
 using Base.Test
 
 @test chomp(readstring(`$(nodejs_cmd()) -v`)) == "v6.10.3"
-@test chomp(readstring(`$(nodejs_cmd()) $(npm_script()) -v`)) == "3.10.10"
+@test chomp(readstring(`$(npm_cmd()) -v`)) == "3.10.10"
