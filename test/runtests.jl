61395f8d800488df965017afc359d54a1e3b42ad
David Anthoff <anthoff@berkeley.edu>
2017-11-17 21:09:20 -0800

    Update Node to 8.9.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using Compat
 using NodeJS
 using Base.Test
 
-@test chomp(readstring(`$(nodejs_cmd()) -v`)) == "v6.10.3"
-@test chomp(readstring(`$(npm_cmd()) -v`)) == "3.10.10"
+@test chomp(readstring(`$(nodejs_cmd()) -v`)) == "v8.9.1"
+@test chomp(readstring(`$(npm_cmd()) -v`)) == "5.5.1"
