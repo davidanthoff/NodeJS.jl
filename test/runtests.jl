a0f717fcdcebe5ac4377ea8afad5bf7d676d5e96
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2017-12-19 16:05:20 +0000

    Add also to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using Compat
 using NodeJS
 using Base.Test
 
-@test chomp(readstring(`$(nodejs_cmd()) -v`)) == "v8.9.1"
+@test chomp(readstring(`$(nodejs_cmd()) -v`)) == "v8.9.3"
 @test chomp(readstring(`$(npm_cmd()) -v`)) == "5.5.1"
