4189062d0c522d42885d02fe0e7e099a73e03e53
David Anthoff <anthoff@berkeley.edu>
2018-01-02 14:48:18 -0800

    Use testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using Compat
 using NodeJS
 using Base.Test
 
+@testset "NodeJS" begin
+
 @test chomp(readstring(`$(nodejs_cmd()) -v`)) == "v8.9.3"
 @test chomp(readstring(`$(npm_cmd()) -v`)) == "5.5.1"
+
+end
