3fc3e88f581d6d8f093bad4c54dc747a55fd1fee
David Anthoff <anthoff@berkeley.edu>
2019-11-28 14:21:22 -0800

    Fix linux platforms

--- src/NodeJS.jl
+++ src/NodeJS.jl
@@ -17,8 +17,8 @@ const nodejs_path = conditional_nodejs_load()
 const node_exe_name = Sys.iswindows() ? "node.exe" : "node"
 const npm_exe_name = Sys.iswindows() ? "npm.cmd" : "npm"
 
-const node_executable_path = nodejs_path===nothing ? node_exe_name : joinpath(nodejs_path, node_exe_name)
-const npm_executable_path = nodejs_path===nothing ? npm_exe_name : joinpath(nodejs_path, npm_exe_name)
+const node_executable_path = nodejs_path===nothing ? node_exe_name : Sys.iswindows() ? joinpath(nodejs_path, node_exe_name) : joinpath(nodejs_path, "bin", node_exe_name)
+const npm_executable_path = nodejs_path===nothing ? npm_exe_name : Sys.iswindows() ? joinpath(nodejs_path, npm_exe_name) : joinpath(nodejs_path, "bin", npm_exe_name)
 
 """
 Return the full path of the node command.
