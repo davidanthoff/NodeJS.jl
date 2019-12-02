a4e2bf95fef2f1ca82f6ba18d325d7589332ea51
David Anthoff <anthoff@berkeley.edu>
2019-12-02 06:25:08 +0000

    Format files using DocumentFormat

--- src/NodeJS.jl
+++ src/NodeJS.jl
@@ -17,8 +17,8 @@ const nodejs_path = conditional_nodejs_load()
 const node_exe_name = Sys.iswindows() ? "node.exe" : "node"
 const npm_exe_name = Sys.iswindows() ? "npm.cmd" : "npm"
 
-const node_executable_path = nodejs_path===nothing ? node_exe_name : Sys.iswindows() ? joinpath(nodejs_path, node_exe_name) : joinpath(nodejs_path, "bin", node_exe_name)
-const npm_executable_path = nodejs_path===nothing ? npm_exe_name : Sys.iswindows() ? joinpath(nodejs_path, npm_exe_name) : joinpath(nodejs_path, "bin", npm_exe_name)
+const node_executable_path = nodejs_path === nothing ? node_exe_name : Sys.iswindows() ? joinpath(nodejs_path, node_exe_name) : joinpath(nodejs_path, "bin", node_exe_name)
+const npm_executable_path = nodejs_path === nothing ? npm_exe_name : Sys.iswindows() ? joinpath(nodejs_path, npm_exe_name) : joinpath(nodejs_path, "bin", npm_exe_name)
 
 """
 Return the full path of the node command.
@@ -31,7 +31,7 @@ end
 Return the full path of the npm command.
 """
 function npm_cmd()
-    if nodejs_path===nothing
+    if nodejs_path === nothing
         return `npm`
     elseif Sys.iswindows()
         return `$npm_executable_path`
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,12 @@ using Test
 
 @testset "NodeJS" begin
 
-node_v = chomp(read(`$(nodejs_cmd()) -v`, String))
-npm_v = chomp(read(`$(npm_cmd()) -v`, String))
+    node_v = chomp(read(`$(nodejs_cmd()) -v`, String))
+    npm_v = chomp(read(`$(npm_cmd()) -v`, String))
 
-if NodeJS.nodejs_path!==nothing
-    @test node_v == "v12.13.1"
-    @test npm_v == "6.12.1"
-end
+    if NodeJS.nodejs_path !== nothing
+        @test node_v == "v12.13.1"
+        @test npm_v == "6.12.1"
+    end
 
 end
